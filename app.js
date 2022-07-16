faeb1a401cb043f558e0d02e2d94a25ea1386c18
AKASH KUMAR <akash292984@gmail.com>
2022-07-16 10:48:27 +0530

    mogoose 01

---
+++ app.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/fruitsDB');
+
+const fruitSchema = new mongoose.Schema({
+    name : String,
+    rating : Number,
+    review : String
+});
+
+const Fruity = mongoose.model("Fruit",fruitSchema);  // changes "Fruit" to its plural form  and lowercase the letter also ie "fruits" when u run it 
+
+const fruit = new Fruity({
+    name : "Apple",
+    rating : 7,
+    review : "very good"
+});
+
+const personschema = new mongoose.Schema({
+    name : String,
+    age : Number
+});
+
+const aadmi  = mongoose.model("Person",personschema);   // changes "person" to its plural form ie "people" when u run it 
+
+const person = new aadmi({
+    name : "Akash_Kumar",
+    age : 20
+});
+person.save();
+
+// person.save().then(
+//     () => console.log('chl gya')
+// );
+
+const kiwi = new Fruity({
+    name : "kiwi",
+    rating : 10,
+    review : "awesom"
+});
+const banana = new Fruity({
+    name : "banana",
+    rating : 8,
+    review : "long "
+});
+const orange = new Fruity({
+    name : "orange",
+    rating : 5,
+    review : "colorful"
+});
+
+Fruity.insertMany([kiwi,banana,orange],function(err){
+    if(err)
+        console.log(err);
+    else
+        console.log("run succesfully");
+});
