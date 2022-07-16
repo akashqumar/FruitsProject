54f9067b0ad98127b6c2a73e98293182ad9d354b
AKASH KUMAR <akash292984@gmail.com>
2022-07-16 11:42:53 +0530

    lc 03

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fruitsDB');
 
 const fruitSchema = new mongoose.Schema({
-    name : String,
-    rating : Number,
+    name : 
+    {
+        type : String,
+        required : [true,"ye to apun ko chahiye he"]
+    },
+    rating : {
+        type : Number,
+        min : 1,
+        max : 10
+    },
     review : String
 });
 
@@ -11,9 +19,11 @@ const Fruity = mongoose.model("Fruit",fruitSchema);  // changes "Fruit" to its p
 
 const fruit = new Fruity({
     name : "Apple",
-    rating : 7,
-    review : "very good"
+    rating : 10,
+    review : "without name nya apple very good"
 });
+
+fruit.save();
 
 const personschema = new mongoose.Schema({
     name : String,
@@ -32,21 +42,21 @@ person.save();
 //     () => console.log('chl gya')
 // );
 
-const kiwi = new Fruity({
-    name : "kiwi",
-    rating : 10,
-    review : "awesom"
-});
-const banana = new Fruity({
-    name : "banana",
-    rating : 8,
-    review : "long "
-});
-const orange = new Fruity({
-    name : "orange",
-    rating : 5,
-    review : "colorful"
-});
+// const kiwi = new Fruity({
+//     name : "kiwi",
+//     rating : 10,
+//     review : "awesom"
+// });
+// const banana = new Fruity({
+//     name : "banana",
+//     rating : 8,
+//     review : "long "
+// });
+// const orange = new Fruity({
+//     name : "orange",
+//     rating : 5,
+//     review : "colorful"
+// });
 
 // Fruity.insertMany([kiwi,banana,orange],function(err){     // it saves data everytime we run 
 //     if(err)
@@ -62,8 +72,9 @@ Fruity.find(function(err,fruits){
         //mongoose.connection.close();
 
         fruits.forEach(function(fruit){
-            console.log(fruit.name);
+            console.log(fruit);
         });
     
         mongoose.connection.close();
 });
+//mongoose.connection.close();
