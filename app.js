ea503ffc4d8018dc6bc03bc468679ef0bb6e222f
AKASH KUMAR <akash292984@gmail.com>
2022-07-17 11:24:37 +0530

    lec 05

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/fruitsDB');
 
 const fruitSchema = new mongoose.Schema({
-    name : String,
-    // {
-    //     type : String,
-    //     required : [true,"ye to apun ko chahiye he"]
-    // },
+    name : 
+    {
+        type : String,
+        required : [true,"ye to apun ko chahiye he"]
+    },
     rating : {
         type : Number,
         min : 1,
@@ -18,25 +18,34 @@ const fruitSchema = new mongoose.Schema({
 const Fruity = mongoose.model("Fruit",fruitSchema);  // changes "Fruit" to its plural form  and lowercase the letter also ie "fruits" when u run it 
 
 const fruit = new Fruity({
-    //name : "Apple",
+    name : "Apple",
     rating : 10,
     review : "without name nya apple very good"
 });
 
-//fruit.save();
+const pinapple = new Fruity({
+    name : "pinapple",
+    rating : 5,
+    review : "it taste so well"
+});
+
+
+pinapple.save();
 
 const personschema = new mongoose.Schema({
     name : String,
-    age : Number
+    age : Number,
+    favouriteFruit : fruitSchema
 });
 
 const aadmi  = mongoose.model("Person",personschema);   // changes "person" to its plural form ie "people" when u run it 
 
 const person = new aadmi({
-    name : "Akash_Kumar",
-    age : 20
+    name : "badal_kumar",
+    age : 18,
+    favouriteFruit : pinapple
 });
-//person.save();
+person.save();
 
 // person.save().then(
 //     () => console.log('chl gya')
@@ -64,40 +73,54 @@ const person = new aadmi({
 //     else
 //         console.log("run succesfully");
 // });
-Fruity.find(function(err,fruits){
+// Fruity.find(function(err,fruits){
+//     if(err)
+//         console.log(err);
+//     else
+//         //console.log(fruits);
+//         //mongoose.connection.close();
+
+//         fruits.forEach(function(fruit){
+//             console.log(fruit);
+//         });
+    
+//         mongoose.connection.close();
+// });
+
+aadmi.find(function(err,people){
     if(err)
         console.log(err);
     else
         //console.log(fruits);
         //mongoose.connection.close();
 
-        fruits.forEach(function(fruit){
+        people.forEach(function(fruit){
             console.log(fruit);
         });
     
-        mongoose.connection.close();
+        //mongoose.connection.close();
 });
 
 
-Fruity.updateOne({_id : "62d39a5982f630751cb748e5"},{name : "apple ka bap"},function(err){
-    if(err)
-        console.log(err);
-    else
-        console.log("success run");
-});
+// Fruity.updateOne({_id : "62d39a5982f630751cb748e5"},{name : "apple ka bap"},function(err){
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log("success run");
+// });
 
-Fruity.deleteOne({name : "orange"},function(err){
-    if(err)
-        console.log(err);
-    else
-        console.log("success run for deleting orange");
+// Fruity.deleteOne({name : "Apple"},function(err){
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log("success run for deleting orange");
 
-});
+// });
 
-Fruity.deleteMany({name : "Apple"},function(err){
-    if(err)
-        console.log(err);
-    else
-        console.log("success run");
+// aadmi.deleteMany({name : "Akash_Kumar"},function(err){
+//     if(err)
+//         console.log(err);
+//     else
+//         console.log("success run for deleting orange");
 
-});
+// });
